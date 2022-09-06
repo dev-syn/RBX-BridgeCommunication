0f30fe26282a267278ff600c86fd360d82d109b9
dev-syn <103451081+dev-syn@users.noreply.github.com>
2022-09-05 20:58:55 -0400

    Add Test Files

--- TestScripts/testServer.server.lua
+++ TestScripts/testServer.server.lua
@@ -1,4 +1,4 @@
 local BridgeCommunicationModule: ModuleScript? = game:GetService("ReplicatedStorage"):FindFirstChild("BridgeCommunication");
 local Dependencies: Folder? = BridgeCommunicationModule:FindFirstChild("Dependencies");
-local TestEZ = require(Dependencies:FindFirstChild("testez"));
+local TestEZ = require(Dependencies:FindFirstChild("TestEZ"));
 TestEZ.TestBootstrap:run({BridgeCommunicationModule});
